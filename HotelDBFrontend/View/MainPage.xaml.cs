f0d6c5e0a104401eb2dd1a665d57c16ef13d55a5
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2017-03-29 13:22:11 +0200

    +DefaultBurgerMenu

--- HotelDBFrontend/View/MainPage.xaml.cs
+++ HotelDBFrontend/View/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace HotelDBFrontend
         {
             this.InitializeComponent();
         }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
     }
 }
